6ca930c418ea177872142cd09ad4308142db189a
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2017-09-14 21:47:23 +0800

    created header.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-I am yosuke!!!
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Real Life</title>
+</head>
+	<?php
+		include('parts/header.php');
+	?>
 
-helloworld!!Fuck You!!!!
-Just kidding!!! hahaha!!
+</html>
---
+++ parts/header.php
@@ -0,0 +1,44 @@
+<head>
+	<meta charset="utf-8">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="lib/boostrap/css/bootstrap.css">
+	<script src="lib/boostrap/js/bootstrap.js" ></script>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#Navbar" aria-expanded="false">
+					<span class="sr-only">メニュー</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button> 
+				<a class="navbar-brand" style="color: #FFBF00;" href="index.php">RealLife</a>
+			</div>
+
+
+
+			<div id="Navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="">Home</a></li>
+					<li><a href="">MyPage</a></li>
+
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a class="dropdown-toggle" data-toggle="dropdown" href="#">LogIn<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">My Page</a></li>
+							<li><a href="#">Registration</a></li>
+
+						</ul>
+					</li>
+				</ul>
+
+			</div>
+		</nav>
+	</div>
+</body>
