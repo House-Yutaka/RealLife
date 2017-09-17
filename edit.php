d7e8500974601e87eb969902262a2b265877b2fa
sakiss64 <wobniarehtrevo0604@gmail.com>
2017-09-17 15:41:56 +0800

    追加します

---
+++ edit.php
@@ -0,0 +1,27 @@
+<?php
+	$eTabName = '';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>会員情報編集</title>
+</head>
+<body>
+	<?php
+	include('parts/header.php');
+	?>
+	<div class="wrapper">
+			<!-- このdivたぐの中に書く -->
+
+
+
+
+		<div class="push"></div>
+	</div>
+
+	<?php
+	include('parts/footer.php');
+	?>
+</body>
+</html>
