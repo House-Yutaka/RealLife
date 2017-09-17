9a431304173a5173ebb1048a8e22f53ddff73cde
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2017-09-17 12:40:33 +0900

    fixed bugs

--- parts/header.php
+++ parts/header.php
@@ -36,7 +36,7 @@
 						<?php break; 
 						case 'mypage':?>
 						<li><a href="" class="faa-parent animated-hover"><i class="fa fa-home fa-fw faa-ring" aria-hidden="true"></i>Home</a></li>
-						<li class="selectedTab"><a href="" class="faa-parent animated-hover selectedTab"><i class="fa fa-meh-o fa-fw faa-ring" aria-hidden="true"></i>Mypage</a></li>		
+						<li><a href="" class="faa-parent animated-hover selectedTab"><i class="fa fa-meh-o fa-fw faa-ring" aria-hidden="true"></i>Mypage</a></li>		
 						<?php break;
 						default:?>
 						<li><a href="" class="faa-parent animated-hover"><i class="fa fa-home fa-fw faa-ring" aria-hidden="true"></i>Home</a></li>
