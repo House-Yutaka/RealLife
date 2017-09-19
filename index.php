686b58932767024e16597cbb53feb18740a64d56
yosukeplus <zeroline.co.jp@outlook.jp>
2017-09-19 21:04:40 +0900

    Try searching!!!!

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Real Life</title>
 	<link rel="stylesheet" type="text/css" href="styles/style_index.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- google map API -->
+	<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBwDNqfLmMufOTLpgGEYoS8b3jrYbtlf-A"></script>
 	
 </head>
 <body>
@@ -15,26 +20,31 @@
 		include('parts/header.php');
 	?>
 	<div class="wrapper">
-		<!-- このdivたぐの中に書く -->
 		<div class="container">
 			<div class="row">
 				<div class="back-style">
 					<div class="col-offset-lg-1 col-lg-11">
-						<div class="cover">世界地図</div>
+						<div class="row">
+							<div class="col-lg-12">
+								<form action="">
+            						<input type="text" value="東京スカイツリー" id="address">
+            						<input type="button" value="地図検索" id="button" onclick="getLatLng(document.getElementById('address').value); return(false);">
+        						</form>
+								<!-- 地図を表示させる要素 -->
+								<div id="map-canvas"></div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-
-
-
-
 		<div class="push"></div>
 	</div>
 
 	<?php
 		include('parts/footer.php');
 	?>
-	<script src="index.js"></script>
+
+	<script type="text/javascript" src="scripts/index.js"></script></div>
 </body>
 </html>
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,60 @@
-HelloWorld
-こんにちは
-こんばんは
-明日も早起き！！
+// HTML要素を変換
+          var mapDiv = document.getElementById( "map-canvas" ) ;
+          // 下記がアクセス時に反映される場所と解像度
+          var map = new google.maps.Map( mapDiv, {
+              center: new google.maps.LatLng( 35.7100, 139.8107 ) ,
+              zoom: 5 ,
+          });
+
+          function getLatLng(place) {
+              // ジオコーダのコンストラクタ
+              var geocoder = new google.maps.Geocoder();
+              // geocodeリクエストを実行。
+              // 第１引数はGeocoderRequest。
+              // 第２引数はコールバック関数。
+              geocoder.geocode({
+                address: place
+              }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                  var bounds = new google.maps.LatLngBounds();
+            
+                  for (var i in results) {
+                    if (results[i].geometry) {
+            
+                      // 緯度経度を取得
+                      var latlng = results[i].geometry.location;
+                      // 住所を取得
+                      var address = results[i].formatted_address.replace(/^日本, /, '');
+                      bounds.extend(latlng);
+            
+                      new google.maps.InfoWindow({
+                        content: address + "<br>(Lat, Lng) = " + latlng.toString()
+                      }).open(map, new google.maps.Marker({
+                        position: latlng,
+                        map: map
+                      }));
+                    }
+                  }
+            
+                  // 範囲を移動
+                  map.fitBounds(bounds);
+                  // ズーム値を手動で設定
+                  map.setZoom(17);
+            
+                } else if (status == google.maps.GeocoderStatus.ERROR) {
+                  alert("GoogleMap 通信エラー 505");
+                } else if (status == google.maps.GeocoderStatus.INVALID_REQUEST) {
+                  alert("GoogleMap リクエストエラー 506 ¥n該当の場所は検索できません。");
+                } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+                  alert("GoogleMap リクエストエラー 507 ¥n時間を置いて再度、検索してください");
+                } else if (status == google.maps.GeocoderStatus.REQUEST_DENIED) {
+                  alert("GoogleMap リクエストエラー 508 ¥nリクエストが拒否されました");
+                } else if (status == google.maps.GeocoderStatus.UNKNOWN_ERROR) {
+                  alert("GoogleMap エラー 509 ¥n予期せぬエラーが発生しました");
+                } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+                  alert("GoogleMap エラー 510 ¥n該当の場所は検索できません");
+                } else {
+                  alert("GoogleMap エラー 509 ¥n予期せぬエラーが発生しました");
+                }
+              });
+            }
