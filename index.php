73c273aa77c60b32d8f807d3bd2f3dccdde37863
yosukeplus <zeroline.co.jp@outlook.jp>
2017-09-19 20:27:45 +0900

    Design Test

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<link rel="stylesheet" type="text/css" href="styles/style_index.css">
 	<meta charset="utf-8">
 	<title>Real Life</title>
+	<link rel="stylesheet" type="text/css" href="styles/style_index.css">
 	
 </head>
 <body>
@@ -15,8 +15,16 @@
 		include('parts/header.php');
 	?>
 	<div class="wrapper">
-			<!-- このdivたぐの中に書く -->
-			<h1>hitomi</h1>
+		<!-- このdivたぐの中に書く -->
+		<div class="container">
+			<div class="row">
+				<div class="back-style">
+					<div class="col-offset-lg-1 col-lg-11">
+						<div class="cover">世界地図</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
 
@@ -27,5 +35,6 @@
 	<?php
 		include('parts/footer.php');
 	?>
+	<script src="index.js"></script>
 </body>
 </html>
