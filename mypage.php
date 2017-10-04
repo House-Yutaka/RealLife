43a539185c4d992e457c7d6ce7b888491d4e4be2
sakiss64 <wobniarehtrevo0604@gmail.com>
2017-10-04 15:42:20 +0800

    mypage変更

--- mypage.php
+++ mypage.php
@@ -2,6 +2,33 @@
 	$eTabName = 'Mypage';
 ?>
 
+<!-- ログイン状態のチェック不可 -->
+
+
+
+<?php
+	session_start();
+	require('parts/db_connect.php');
+
+	// ログイン状態かチェック
+	// if(!isset($_SESSION['seego_info']['id'])){
+	// 	header('Location: index.php');
+	// 	exit();
+	// }
+
+	if(isset($_SESSION['seego_info']['id'])){
+
+		$sql = 'SELECT * FROM `seego_users` WHERE `id`=?';
+		$data = array($_SESSION['seego_info']['id']);
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute($data);
+		$record = $stmt->fetch(PDO::FETCH_ASSOC);
+      	header('Location: mypage.php');
+      	exit();
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,69 +39,70 @@
 	<?php
 		include('parts/header.php');
 	?>
+	<form method="POST" action="" enctype="multipart/form-data">
 	<div class="wrapper">
 			<!-- このdivたぐの中に書く -->
-			<div class="container" align="center">
-				<div class="userinfo">
-					<div class="row">					
-						<div class="col-lg-12">												
-							<img class="userpic" alt="User Pic" src="images/CIMG0388.JPG"><br>
-						<div class="form">
-							<label>Name</label><br>
-							<input class="form-control" type="text" name="nickname" required style="width: 300px; border: solid 2px #001a42 ">
-							<label>一言</label><br>                       
-                            <textarea class="form-control" name="content" rows="10" required style="width: 300px; border: solid 2px #001a42 "></textarea>
-           <a href="signup.php" class="btn btn-danger btn-xs">ログアウト</a>
+		<div class="container">
+			<div class="row">					
+				<div class="col-xs-3">
+		  			<div class="prof-img">	  
+						ここ編集中！！
+						<!-- アイコン写真が飛んでくる -->
+						<?php if($_SESSION['seego_info']['user_icon']==null){ ?>
+							<img src="images/images.png">
+						<?php }else{ ?>
+							<img src="images/<?php echo $_SESSION['seego_info']['user_icon'];?>">
+						<?php } ?>
+	  					 <div>
+	  					 	<!-- usernameが飛んでくる -->
+	     					<h2><?php echo $_SESSION['seego_info']['username']; ?></h2>
+	  					 </div>
+						 <div>
+	 					  <a href="edit.php" type="button"><i class="fa fa-edit"></i>Edit</a>
+	 					  <a href="signup.php" type="button"><i class="fa sign-out"></i>LogOut</a>
+						 </div>
+					</div>
+				</div>
+					
+				<div class="col-xs-9">
+					<div class="row">
+						 <!-- 縦に記述 -->
+						<h3>投稿一覧</h3><br>
+						<!-- 投稿した写真＆コメントが表示される -->
+						<div class="col-xs-3">
+						<div>
+							<img width="180px" height="180px" src="images/IMG_1592.JPG" alt="">
+						</div><br>
+						
+						<div>
+							<img width="180px" height="180px" src="images/city.jpg" alt="">
+						</div><br>
+						
+						<h3>お気に入り一覧</h3><br>
+						<!-- いいね！押した記事が表示される -->
+						<div>
+							<img width="180px" height="180px" src="images/IMG_0427.JPG" alt="">
+						</div><br>
+						
+						<div>
+							<img width="180px" height="180px" src="images/CIMG0388.JPG" alt="">
+						</div><br>
+						</div>
 
-						</div>	
-						</div>						
-					</div>					
-				</div>				
-			</div>
-
-			
-			<div class="container">			
-				<div class="row">
-				<h2>投稿一覧</h2><br>
-					<!-- 投稿した写真＆コメントが表示される -->
-					<div class="col-xs-3">
-						<img width="180px" height="180px" src="images/IMG_1592.JPG" alt="">
-					</div>
-					<div class="col-xs-9" align="center">
-						<textarea class="contribution" required style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea>						
-					</div>
-					<div class="col-xs-3">
-						<img width="180px" height="180px" src="images/city.jpg" alt="">
-					</div>
-					<div class="col-xs-9" align="center">
-						<textarea class="contribution" required style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea>						
-					</div>
-
-				</div>											
-			</div>
-
-			<div class="container">			
-				<div class="row">
-				<h2>お気に入り一覧</h2><br>
-					<!-- いいね！押した記事が表示される -->
-					<div class="col-xs-3" align="left">
-						<img width="180px" height="180px" src="images/IMG_0427.JPG" alt="">
-					</div>
-					<div class="col-xs-9" align="center">
-						<textarea class="contribution" required style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea>						
-					</div>
-					<div class="col-xs-3" align="left">
-						<img width="180px" height="180px" src="images/CIMG0388.JPG" alt="">
-					</div>
-					<div class="col-xs-9" align="center">
-						<textarea class="contribution" required style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea>						
-					</div>
-				</div>											
-			</div>
-
-
+						<div class="col-xs-9">
+							<textarea class="contribution" style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea><br>
+							<textarea class="contribution" style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea><br><br><br>
+							<textarea class="contribution" style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea><br>
+							<textarea class="contribution" style="width: 85%; height: 180px; border: solid 2px #001a42 " rows="5"></textarea>
+						</div>
+				</div>	
+				</div>	
+			</div>									
+		</div>
 
 
+
+	</form>
 		<div class="push"></div>
 	</div>
 
