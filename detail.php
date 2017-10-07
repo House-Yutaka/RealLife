47879c77609c084203b5091eb2ef2cb1108b462e
yosukeplus <zeroline.co.jp@outlook.jp>
2017-10-07 22:38:20 +0900

    更新

--- detail.php
+++ detail.php
@@ -1,17 +1,37 @@
 <?php
+$eTabName = 'detail';
+session_start();
+// require('db_connect.php');
+
+// if(!isset($_SESSION['login_user']['id'])){
+
+//     header('Location: login.php');
+//     exit();
+// }
+
+
 
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>detsil</title>
 	<link rel="stylesheet" type="text/css" href="styles/style_detail.css">
 	<link rel="stylesheet" type="text/css" href="lib/boostrap/css/bootstrap.css">
 </head>
 <body>
+	<?php
+  		include('parts/header.php');
+  	?>
+
+
+
+  	<?php
+  		include('parts/footer.php');
+  	?>
 
 </body>
 </html>
