4f0d3c6057c66d7809e85f01943ebdd91e458610
yosukeplus <zeroline.co.jp@outlook.jp>
2017-10-08 02:20:08 +0900

    寝る前プッシュ

--- detail.php
+++ detail.php
@@ -23,15 +23,30 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="lib/boostrap/css/bootstrap.css">
 </head>
 <body>
-	<?php
-  		include('parts/header.php');
-  	?>
+<?php
+  include('parts/header.php');
+?>
+
+<div class="container">
+	<div class="row">
+		<!-- <div class="col-xs-offset-2"></div> -->
+		<div class="col-lg-offset-2 col-lg-8">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="back-style">
+						<h1 style="text-align: center;">Hello.World</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-offset-2"></div>
+	</div>
+</div>
 
 
-
-  	<?php
-  		include('parts/footer.php');
-  	?>
+<?php
+  include('parts/footer.php');
+?>
 
 </body>
 </html>
