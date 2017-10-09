e25a95e9a55ce19fac98cad284e03118b6531593
yosukeplus <zeroline.co.jp@outlook.jp>
2017-10-10 02:12:23 +0900

    detail

--- detail.php
+++ detail.php
@@ -4,24 +4,31 @@ require('parts/db_connect.php');
 
 session_start();
 
-if(!isset($_SESSION['login_user']['id'])){
-    header('Location: login.php');
-    exit();
-}
 
-// ここはログインしたユーザーが通る
-// パラメータチェック
 if(!isset($_GET['id'])){
   header('Location: timeline.php');
   exit();
 }
-  $sql = 'SELECT friends.id, name, area_id, gender, created, modified, area FROM friends,areas WHERE friends.area_id=areas.id';
+
+  $sql ='SELECT `user_id`,`text`,`address`,`picture_path` FROM `seego_pictures` WHERE `id`=?';
   $data = array($_GET['id']);
   $stmt = $dbh->prepare($sql);
   $stmt->execute($data);
 
-  // 1件のみなので、Whileでループさせず、一件目のみFetchする
-  $record = $stmt->fetch(PDO::FETCH_ASSOC);
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+  
+
+  // var_dump($rec);
+  // echo '<br>';
+  // echo '<br>';
+
+  $sql ="SELECT `seego_pictures`.`id`,`user_id`,`username` FROM `seego_pictures`,`seego_users` WHERE `seego_pictures`.`user_id` = `seego_users`.`id`";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
+
+  
+
+	
 
 ?>
 
@@ -45,7 +52,29 @@ if(!isset($_GET['id'])){
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="back-style">
-						
+						<div style="text-align: center; margin-top: 20px;">
+				    		<img src="images/ex_view_images/<?php echo $rec['picture_path'];?>" width="90%">
+				    	</div>
+				   		<br>
+						<?php
+							$data=array();
+    						while (1) {
+    							$rec2 = $stmt->fetch(PDO::FETCH_ASSOC);
+     							if ($rec2 == false) {
+         							break;
+    							}
+
+    							if($rec2['id'] == $_GET['id']){
+									$rec2['username'];
+    							?>
+								<h1><?php echo $rec2['username']; ?></h1>
+
+								<?php } ?>
+						<?php } ?>
+						<br>
+						<h3><?php echo $rec['text']; ?></h3>
+						<br>
+						<h3><?php echo $rec['address']; ?></h3>
 					</div>
 				</div>
 			</div>
