3cec693cc0ffd8a8b6ff2e8507df3049c9040af8
yosukeplus <zeroline.co.jp@outlook.jp>
2017-10-11 15:46:19 +0900

    detail終わり

--- detail.php
+++ detail.php
@@ -1,34 +1,77 @@
 <?php
 $eTabName = 'detail';
-require('parts/db_connect.php');
-
-session_start();
-
 
 if(!isset($_GET['id'])){
-  header('Location: timeline.php');
+  header('Location: index.php');
   exit();
 }
 
-  $sql ='SELECT `user_id`,`text`,`address`,`picture_path` FROM `seego_pictures` WHERE `id`=?';
-  $data = array($_GET['id']);
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($data);
+session_start();
+require('parts/db_connect.php');
 
-  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-  
+$sql ='SELECT `id`,`user_id`,`text`,`address`,`picture_path` FROM `seego_pictures` WHERE `id`=?';
+$data = array($_GET['id']);
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
 
-  // var_dump($rec);
-  // echo '<br>';
-  // echo '<br>';
+  $contribution = $stmt->fetch(PDO::FETCH_ASSOC);
 
-  $sql ="SELECT `seego_pictures`.`id`,`user_id`,`username` FROM `seego_pictures`,`seego_users` WHERE `seego_pictures`.`user_id` = `seego_users`.`id`";
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($data);
+// echo '<pre>';
+// var_dump($contribution);
+// echo '</pre>';
 
-  
+$sql ="SELECT `seego_pictures`.`id`,`user_id`,`username` 
+  	   FROM `seego_pictures`,`seego_users` 
+  	   WHERE `seego_pictures`.`user_id` = `seego_users`.`id`";
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);	
 
-	
+$userdatas=array();
+while (1) {
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	if ($rec == false) {
+		break;
+	}
+
+	$userdatas[] = $rec;
+}
+
+
+if (!empty($_POST)) {
+	if(!isset($_SESSION['login_user']['id'])){
+    header('Location: login.php');
+    exit();
+	}
+
+	if(isset($_POST['likes']) && $_POST['likes'] == 'like'){
+        
+        $sql = 'INSERT INTO `seego_likes` SET `seego_pictures_id`=? ,
+                                        	  `user_id`=?
+        ';
+        $data = array($_POST['seego_pictures_id'],$_SESSION['login_user']['id']);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+    }elseif(isset($_POST['likes']) && $_POST['likes'] == 'unlike'){
+        //いいね取り消しを押した場合、ここの処理が走る
+        $sql = 'DELETE FROM `seego_likes` WHERE `seego_pictures_id`=?
+                                    		AND `user_id`=?
+        ';
+        $data = array($_POST['seego_pictures_id'],$_SESSION['login_user']['id']);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+    }
+    
+    if (isset($_POST['likes'])) {
+
+      header('Location: detail.php?id='.$_POST['seego_pictures_id']);
+      exit();
+    }
+
+}
+
 
 ?>
 
@@ -48,38 +91,67 @@ if(!isset($_GET['id'])){
 <div class="container">
 	<div class="row">
 		<!-- <div class="col-xs-offset-2"></div> -->
-		<div class="col-lg-offset-2 col-lg-8">
+		<div class="col-lg-offset-3 col-lg-6">
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="back-style">
-						<div style="text-align: center; margin-top: 20px;">
-				    		<img src="images/ex_view_images/<?php echo $rec['picture_path'];?>" width="90%">
+						<div class="image">
+				    		<img src="images/ex_view_images/<?php echo $contribution['picture_path'];?>" width="100%">
 				    	</div>
-				   		<br>
-						<?php
-							$data=array();
-    						while (1) {
-    							$rec2 = $stmt->fetch(PDO::FETCH_ASSOC);
-     							if ($rec2 == false) {
-         							break;
-    							}
+				   		<div class="back-color">
+				   			<div class="row">
+				   				<div class="col-lg-3"></div>
+				   				<div class="col-lg-9">
+						   			<div class="deyail">
+						   				<li style="margin-bottom: 8px;"><?php echo $contribution['address']; ?></li>
+						   				<li style="margin-bottom: 10px;"><?php echo $contribution['text']; ?></li>
+								   	</div>
+									<?php foreach ($userdatas as $userdata) {  ?>
+										<?php if($userdata['id'] == $_GET['id']){  ?>
+											<div class="detail_name"><span style="color: orange;">@</span><?php echo $userdata['username']; ?></div>
+										<?php } ?>
+									<?php } ?>	
+									<?php 
+										  if($contribution != false) {
+						                  // いいねのカウントを表示
+						                  $sql = 'SELECT COUNT(*) AS `count` FROM `seego_likes` WHERE `seego_pictures_id` = ?';
+						                  $data = array($contribution['id']);
+						                  $stmt = $dbh->prepare($sql);
+						                  $stmt->execute($data);
+						                  // 1件文のデータを取得
+						                  $likes = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    							if($rec2['id'] == $_GET['id']){
-									$rec2['username'];
-    							?>
-								<h1><?php echo $rec2['username']; ?></h1>
+						                  // 自分がいいね！を一回以上しているかどうかをチェック
+						                  $sql = 'SELECT COUNT(*) AS `count` FROM `seego_likes` WHERE `seego_pictures_id` = ? AND `user_id` = ?';
+						                  $data = array($contribution['id'],$_SESSION['login_user']['id']);
+						                  $stmt = $dbh->prepare($sql);
+						                  $stmt->execute($data);
+						                  // 1件文のデータを取得
+						                  $likes_chk = $stmt->fetch(PDO::FETCH_ASSOC);
 
-								<?php } ?>
-						<?php } ?>
-						<br>
-						<h3><?php echo $rec['text']; ?></h3>
-						<br>
-						<h3><?php echo $rec['address']; ?></h3>
+						                  // var_dump($likes_chk['count']);
+						              	?>
+						              	<!-- いいね！ボタン設置 -->
+						              	<form method="POST" action="">
+						                	いいね！数:<?php echo $likes['count'];?>
+						                	<input type="hidden" name="seego_pictures_id" value="<?php echo $contribution['id']; ?>">
+						                	<?php if($likes_chk['count'] < 1){ ?>
+						                    <input type="hidden" name="likes" value="like">
+						                    <input type="submit" value="いいね！" class="btn btn-primary btn-xs">
+						                	<?php }else{ ?>
+						                    <input type="hidden" name="likes" value="unlike">
+						                    <input type="submit" value="いいね！取り消し" class="btn btn-danger btn-xs">
+						                <?php } ?>
+						              </form>
+									<?php } ?>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-offset-2"></div>
+		<div class="col-lg-offset-3"></div>
 	</div>
 </div>
 
