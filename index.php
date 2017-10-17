05f702e2699b89237ddc00ac7681a9f3230395e3
yosukeplus <zeroline.co.jp@outlook.jp>
2017-10-17 22:42:47 +0900

    追加

--- index.php
+++ index.php
@@ -22,7 +22,28 @@ require('parts/db_connect.php');
         
     }
 
+$sql='SELECT * FROM `seego_pictures` ORDER BY `seego_pictures`.`id` DESC';
+    $data=array();
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute($data);
+    // $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $userditail=$record;
 
+
+    $userditails = array();
+    while(true){
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+        // $recordはデータベースのカラム値をkeyとする。
+        // 連想配列で構成されます。（データベースから１件取ってきます。）
+        if(!$record){
+            break;
+        }
+        $userditails[]=$record;
+    }
+
+    // echo '<pre>';
+    // var_dump($userditail[1]['picture_path']);
+    // echo '</pre>';
 
 // ３．データベースを切断する
 $dbh = null;
@@ -41,7 +62,7 @@ $dbh = null;
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>SEEGO</title>
 	<link rel="stylesheet" type="text/css" href="styles/style_index.css">
-	<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="lib/boostrap/css/bootstrap.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<!-- google map API -->
 	<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBwDNqfLmMufOTLpgGEYoS8b3jrYbtlf-A"></script>
@@ -54,11 +75,12 @@ $dbh = null;
 	<div class="wrapper">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-offset-1"></div>
-				<div class="back-style">
-					<div class="col-lg-11">
+				<!-- <div class="col-lg-offset-1"></div> -->
+				<!-- <div class="back-style"> -->
+				<div class="col-lg-12">
+					<div class="back-style">
 						<div class="row">
-							<div class="col-lg-offset-1 col-lg-11">
+							<div class="col-lg-offset-1 col-lg-10">
 								 <form action="">
             						<input type="text" id="address" placeholder="例 :東京スカイツリー">
             						<input type="button" value="地図検索" id="button" onclick="getLatLng(document.getElementById('address').value); return(false);">
@@ -67,10 +89,32 @@ $dbh = null;
 								<!-- 地図を表示させる要素 -->
 								<div id="map-canvas"></div>
 							</div>
+							<div class="col-lg-offset-1"></div>
 						</div>
 					</div>
 				</div>
 			</div>
+			<div class="row">
+		        <!-- <div class="col-lg-offset-1"></div> -->
+		        <div class="col-lg-12">
+		        	<div class="back-style2">
+		                <div class="row">
+		                	<div class="col-lg-offset-1 col-lg-10">
+		                        <?php foreach($userditails as $userditail){ ?>
+		                            <div class="col-offset-lg-1"></div>
+		                            <div class="col-lg-3 col-offset-lg-1">
+		                             	<a href="detail.php?id=<?php echo $userditail['id']; ?>"><img src="images/ex_view_images/<?php echo $userditail['picture_path'];?>" width="100%" height="200px"></a>
+		                                 <div class="back-color2">
+		                                    <h6><?php echo $userditail['address']; ?></h6>
+		                                </div>
+		                            </div>
+		                        <?php } ?>
+		                    </div>
+		                    <div class="col-lg-offset-1"></div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
 		</div>
 		<div class="push"></div>
 	</div>
